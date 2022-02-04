74127e83d143ec0965d665eaa13340b7f27c79f9
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-02-04 20:14:11 -0300

    Adicionei primeiro exercício do dia

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-1.js
@@ -0,0 +1,39 @@
+// Exercício de fatorar
+
+let fator = 10;
+let fatorado = fator;
+for (let id = 1; id < fator; id += 1) {
+    fatorado *= id
+};
+console.log(fatorado)
+
+// Exercício de inverter palavra
+
+let word = 'tryber';
+let letras = [];
+let resultado;
+for (let i = word.length -1; i >= 0; i -= 1) {
+    letras.push(word[i]);
+};
+resultado = letras.join('');
+
+console.log(resultado)
+
+// Descobrir qual a maior string em um array e a menor
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorString = array[0];
+let menorString = array[0];
+for (let i = 0; i < array.length; i += 1) {
+if(array[i].length >= maiorString.length) {
+    maiorString = array[i]
+}
+if(array[i].length <= menorString.length) {
+    menorString = array[i]
+}
+}
+
+console.log(maiorString, menorString)
+
+// Descobrir maior número primo entre 0 e 50
+
