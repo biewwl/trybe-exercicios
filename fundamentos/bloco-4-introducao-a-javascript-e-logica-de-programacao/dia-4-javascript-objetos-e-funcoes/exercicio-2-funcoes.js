29d98d614bafcec3872a77757a9af0a6b421f096
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-02-08 14:05:26 -0300

    Terminei os exercícios do dia 4.4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-2-funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-2-funcoes.js
@@ -72,3 +72,74 @@ function maiorPalavra(arrayDePalavras) {
 }
 
 console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+function maisVezesRepetido(num) {
+    let contador = 0;
+    let contadorMaior = 0;
+    let maisRepetido = 0;
+
+    for (let i = 0; i < num.length; i += 1) {
+    contador = 0;
+        for (let id = 0; id < num.length; id += 1) {
+            if (num[i] === num[id]) {
+                contador += 1;
+            }
+        }
+        if (contador > contadorMaior) {
+        contadorMaior = contador;
+        maisRepetido = num[i]
+        }
+    }
+    return maisRepetido
+}
+
+
+console.log(maisVezesRepetido([2, 3, 2, 5, 8, 2, 3]));
+
+// 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+function somarAteNumero(numero) {
+    let resultado = 0;
+    for (let id = 0; id <= numero; id += 1) {
+        resultado += id;
+    }
+    return resultado;
+}
+
+console.log(somarAteNumero(5))
+
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+
+function verificaFimPalavra(palavra, fim) {
+    let final = false;
+    let palavraInvertida = [];
+    let fimInvertido = [];
+    let reverseWord;
+    let reverseEnd;
+    
+    for (let id = palavra.length - 1; id >= 0; id -= 1) {
+        palavraInvertida.push(palavra[id])
+        reverseWord = palavraInvertida.join('')
+    }
+    for (let ix = fim.length - 1; ix >= 0; ix -= 1)  {
+        fimInvertido.push(fim[ix])
+        reverseEnd = fimInvertido.join('')
+    }
+    let ultimaV = true;
+    for (let op = 0; op < reverseEnd.length; op += 1) {
+        for (let num = 0; num < reverseEnd.length; num += 1) {
+            if (reverseEnd[num] === reverseWord[num] && ultimaV === true) {
+                final = true
+            } else {
+                ultimaV = false
+            }
+        }
+    }
+
+    return ultimaV;
+}
+
+console.log(verificaFimPalavra('trybe', 'be'));
+console.log(verificaFimPalavra('joaofernando', 'fernan'))
