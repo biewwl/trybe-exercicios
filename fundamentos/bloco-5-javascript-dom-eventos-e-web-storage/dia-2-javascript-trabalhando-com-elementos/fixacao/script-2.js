9ed7bb1be9e58ad5320411b293995951255666b3
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-02-10 16:00:42 -0300

    Adicionei últimos exercícios de fixação

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao/script-2.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao/script-2.js
@@ -31,3 +31,63 @@ document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
 // Agora acesse o 'terceiroFilho' a partir de 'pai'.
 
 document.querySelector('#pai').lastElementChild.previousElementSibling;
+
+// ////////////////////////
+// PARTE 2
+// ////////////////////////
+
+// Crie um irmão para 'elementoOndeVoceEsta'.
+
+let pai = document.querySelector('#pai');
+
+let texto = document.createElement('p');
+texto.innerText = 'Este parágrafo está criado com JavaScript'
+
+pai.appendChild(texto);
+
+// Crie um filho para 'elementoOndeVoceEsta'.
+
+let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+
+let imagem = document.createElement('img');
+imagem.src = './logo-trybe.png'
+
+elementoOndeVoceEsta.appendChild(imagem);
+
+// Crie um filho para 'primeiroFilhoDoFilho'.
+
+let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+
+let link = document.createElement('a');
+link.innerText = 'Instagram biewwl';
+link.href = 'https://instagram.com/biewwl';
+link.target = '_blank';
+link.style.display = 'block';
+link.id = 'linkInstagram';
+
+primeiroFilhoDoFilho.appendChild(link);
+
+// A partir desse filho criado, acesse 'terceiroFilho' .
+
+document.querySelector('#linkInstagram').parentElement.parentElement.nextElementSibling;
+
+// ////////////////////////
+// PARTE 3
+// ////////////////////////
+
+// Remova todos os elementos filhos de 'paiDoPai' exceto 'pai' , elementoOndeVoceEsta e 'primeiroFilhoDoFilho'.
+
+let primeiroFilho = document.querySelector('#primeiroFilho');
+let terceiroFilho = document.querySelector('#terceiroFilho');
+let quartoEUltimoFilho = document.querySelector('#quartoEUltimoFilho');
+let segundoEUltimoFilhoDoFilho = document.querySelector('#segundoEUltimoFilhoDoFilho');
+
+let remove = [primeiroFilho, terceiroFilho, quartoEUltimoFilho];
+
+for (key in remove) {
+    let elementRemove = remove[key];
+
+    pai.removeChild(elementRemove);
+}
+
+elementoOndeVoceEsta.removeChild(segundoEUltimoFilhoDoFilho);
