598c0a1a03eed67eedffb3d59b51470d76d2162b
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-02-21 19:51:12 -0300

    Fim dos exercicios

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios/script.js
@@ -1,3 +1,28 @@
+let botao = document.querySelector('#send');
+
+function stop(e){
+  e.preventDefault();
+}
+
+botao.addEventListener('click', stop);
+
+botao.addEventListener('click', verificaTamanho);
+
+function verificaTamanho() {
+  let tamanho1 = document.querySelector('#name').value.length;
+  let tamanho2 = document.querySelector('#email').value.length;
+  let data = document.querySelector('#date');
+  let fotosOk = document.querySelector('#usage-image');
+
+  if(tamanho1 < 10 || tamanho2 < 10 || data.value === '' || fotosOk.checked !== true) {
+    alert('Dados Inválidos')
+  } else if(tamanho1 >= 10 && tamanho2 >= 10 && data.value !== '' && fotosOk.checked) {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+  }
+}
+
+// Dia 6.2
+
 window.addEventListener('DOMContentLoaded', function()
   {
     let $min = document.querySelector('.real [name="realDPX-min"]');
