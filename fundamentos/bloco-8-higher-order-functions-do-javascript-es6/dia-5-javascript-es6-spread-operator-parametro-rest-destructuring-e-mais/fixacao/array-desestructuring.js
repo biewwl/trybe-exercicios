cc323886c4f1a798a7f882afc892d4975df0375b
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-03-15 09:59:03 -0300

    Realizei exercicios de fixacao 2-4 do dia 8.5

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao/array-desestructuring.js
@@ -0,0 +1,37 @@
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [ texto, imprimir ] = saudacoes;
+
+imprimir(texto);
+
+// 
+// 2
+// 
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[ animal, bebida, comida ] = [comida, animal, bebida]
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+// 
+// 3
+// 
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+// Quanto mais vírgulas mais casas são tiradas do começo do array!
---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao/object-desestructuring.js
@@ -0,0 +1,17 @@
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const { name: nome, age, nationality} = user;
+const { profession, squad, squadInitials } = jobInfos
+
+console.log(`Hi, my name is ${nome}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
